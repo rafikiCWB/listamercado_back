aff65795106662446c048ca3fca363a4e79e7a58
Rafael Grando <sta_rafinha@hotmail.com>
2022-09-20 21:06:32 -0300

    Project booted

---
+++ src/main/java/com/brisaboa/appmercado/CopiaProjetoProfIsidroApplication.java
@@ -0,0 +1,13 @@
+package com.brisaboa.appmercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopiaProjetoProfIsidroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopiaProjetoProfIsidroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brisaboa/appmercado/CopiaProjetoProfIsidroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brisaboa.appmercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopiaProjetoProfIsidroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
